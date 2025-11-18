58dab94fab2fd6ef74101806cbd2bf650757b188
Naliat <tailanoliveira584@gmail.com>
2025-11-18 14:41:40 -0300

    feat(project): adicionar novas pastas e estrutura do frontend

---
+++ backend/models/models.py
@@ -0,0 +1,5 @@
+from odmantic import Model
+
+class User(Model):
+    nome: str
+    email: str
---
+++ frontend/js/registrarRemedio.js
@@ -0,0 +1,3 @@
+document.querySelector(".btn").addEventListener("click", () => {
+    alert("Entrada registrada com sucesso!");
+});
