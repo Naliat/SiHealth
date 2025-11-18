a208155177f07d2efd6f403e20195c8099b157ad
Naliat <tailanoliveira584@gmail.com>
2025-11-18 14:41:27 -0300

    feat(users): adicionar melhorias e ajustes nas rotas de usuário

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 from database import engine
 from models import User
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/")
-async def create_user(user: User):
+async def create_user(nome: str = Form(...), email: str = Form(...)):
+    user = User(nome=nome, email=email)
     await engine.save(user)
     return {"message": "Usuário criado com sucesso!"}
 
